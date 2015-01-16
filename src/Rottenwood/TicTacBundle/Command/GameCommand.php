13a413ddf18d49fe9e8e6984a6ac3f814e98baa3
Petr Karmashev <smonkl@bk.ru>
2015-01-16 12:16:55 +0200

    Отрисовка пустой таблицы исходя из параметров игрового поля

--- src/Rottenwood/TicTacBundle/Command/GameCommand.php
+++ src/Rottenwood/TicTacBundle/Command/GameCommand.php
@@ -24,23 +24,57 @@ class GameCommand extends ContainerAwareCommand {
 
         $output->writeln('Новая игра начинается!');
 
-        for ($y = 1; $y <= Game::BOARD_AXIS_Y; $y++) {
-            $this->drawRow($table, $output);
-        }
+        $this->drawTable($table, $output);
     }
 
     /**
-     * Отрисовка табличной строки
+     * Отрисовка таблицы
      * Некоторые методы консольных хелперов задепрекейтили, но альтернативу пока не дали
      * @param TableHelper     $table
      * @param OutputInterface $output
      */
-    private function drawRow(TableHelper $table, OutputInterface $output) {
+    private function drawTable(TableHelper $table, OutputInterface $output) {
         $imageTic = ' X ';
         $imageTac = ' O ';
-        $imageEmpty = '   ';
+        $imageBorder = '---';
+        $imageSpace = ' ';
+        $letters = range('a', 'z');
 
-        $table->setRows([array_pad([], Game::BOARD_AXIS_X, $imageEmpty)]);
+        $headers = range(1, Game::BOARD_AXIS_X);
+        array_unshift($headers, $imageSpace);
+        $headers = $this->addSpaces($headers, $imageSpace);
+
+        $table->setHeaders($headers);
+
+        $rows = [];
+        for ($i = 1; $i <= Game::BOARD_AXIS_Y; $i++) {
+            $row = array_pad([], Game::BOARD_AXIS_X, $imageSpace);
+            array_unshift($row, $letters[$i - 1]);
+            $rows[] = $this->addSpaces($row, $imageSpace);
+
+            if ($i != Game::BOARD_AXIS_Y) {
+                $borderRow = array_pad([], Game::BOARD_AXIS_X, $imageBorder);
+                array_unshift($borderRow, $imageBorder);
+                $rows[] = $borderRow;
+            }
+        }
+
+        $table->setRows($rows);
         $table->render($output);
+    }
+
+    /**
+     * Добавление "пробелов" ко всем элементам массива
+     * @param array  $array
+     * @param string $imageSpace
+     * @return array
+     */
+    private function addSpaces(array $array, $imageSpace) {
+        return array_map(
+            function ($field) use ($imageSpace) {
+                return $imageSpace . $field . $imageSpace;
+            },
+            $array
+        );
     }
 }
